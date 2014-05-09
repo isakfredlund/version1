b5e717343f6c3a009262ccc5cbc8445cba34a441
isakfredlund <isakfredlund@10.1.2.28>
2014-05-09 11:06:42 +0200

    Test of new Rss. Anslagstavla.mah instead of old news

--- src/se/mah/kd330a/project/adladok/model/Constants.java
+++ src/se/mah/kd330a/project/adladok/model/Constants.java
@@ -2,6 +2,6 @@ package se.mah.kd330a.project.adladok.model;
 
 public class Constants {
  public static final String mahNewsSavedFileName = "mahnews";
- public static final String mahNewsAdress = "http://www.mah.se/Nyheter/RSS/News/";
+ public static final String mahNewsAdress = "http://www.mah.se/Nyheter/RSS/Anslagstavla-fran-Malmo-hogskola/";
  
 }
